8b969a06ab758613f8d9cf956634f129eb9a08f8
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2022-09-12 12:28:22 +0300

    hw-09-task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+// console.log(buttonStart);
+// console.log(buttonStop);
+let bodyCol = null;
+buttonStart.addEventListener('click', () => {
+  bodyCol = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+});
+buttonStop.addEventListener('click', () => {
+  clearInterval(bodyCol);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+});
